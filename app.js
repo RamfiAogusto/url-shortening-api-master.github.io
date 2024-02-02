21b640fd41f280b365f8e67c4f29c56121f53eb4
Ramfi <ramfiaogusto@gmail.com>
2024-02-02 10:53:50 -0800

    ya acorta

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
 const cont = document.querySelector(".cont");
 const toggleMenu = document.querySelector(".toggle-menu");
-
 toggleMenu.addEventListener("click", () => {
-  const hasClass = cont.classList.contains("none");
+    const hasClass = cont.classList.contains("none");
 
-  if (hasClass) {
-    cont.classList.remove("none");
-  } else {
-    cont.classList.add("none");
-  }
-});
-
-
-
-
-/* const form = document.getElementById('my-form');
-async function shortenUrl(e) {
-  e.preventDefault();
-
-  //const url = e.srcElement.childNodes[1].value;
-
-  const url = 'https://free-url-shortener.p.rapidapi.com/API/write/get?url=url&type=type&custom=custom&private=1&via=via&password=password&uses=100';
-  const options = {
-    method: 'GET',
-    headers: {
-      
-      'X-RapidAPI-Key': '',
-      'X-RapidAPI-Host': 'free-url-shortener.p.rapidapi.com'
+    if (hasClass) {
+        cont.classList.remove("none");
+    } else {
+        cont.classList.add("none");
     }
-  };
-  
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+});
+/* ------------- */
+
+function aggAlDocumento(urlToShorten, result) {
+    let containerForm = document.querySelector(".container-form");
+    let acortadoContainer = document.createElement("div");
+    let urlsContainer = document.createElement("div");
+    let urlLarga = document.createElement("p");
+    let urlCorta = document.createElement("a");
+    let btnCopy = document.createElement("button");
+    /* ------ */
+    urlLarga.innerText = urlToShorten;
+    urlLarga.title="Url Larga";
+    urlsContainer.appendChild(urlLarga);
+    urlCorta.innerText = result;
+    urlCorta.href = result;
+    urlCorta.title = "Url Corta";
+    urlsContainer.appendChild(urlCorta);
+    acortadoContainer.appendChild(urlsContainer);
+    urlsContainer.classList = "urls-container";
+    acortadoContainer.classList="acortado-container";
+    btnCopy.innerText = "copy";
+    acortadoContainer.appendChild(btnCopy);
+    containerForm.appendChild(acortadoContainer);
 }
 
+/* ---------------------------------------------- */
+document.getElementById("my-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputUrl = document.getElementById("url");
+    const urlToShorten = inputUrl.value;
+    shortenUrl(urlToShorten);
+});
 
-// Escuchar el evento "submit" del formulario
-form.addEventListener('submit', shortenUrl); */
+async function shortenUrl(urlToShorten) {
+    const apiUrl = "https://url-shortener23.p.rapidapi.com/shorten";
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "X-RapidAPI-Key":
+                "40e18ac248msh039a0f714c16017p110569jsn198bc93d196a",
+            "X-RapidAPI-Host": "url-shortener23.p.rapidapi.com",
+        },
+        body: JSON.stringify({
+            url: urlToShorten,
+        }),
+    };
+    try {
+        const response = await fetch(apiUrl, options);
+        const result = await response.json();
+        console.log(result.short_url);
+        aggAlDocumento(urlToShorten, result.short_url);
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+}
+
+/* ------------------------------------------------------------------- */
